8ce13392e8ae455e7da3d583c5f373d117980303
Tommy <43862943+tommyp6@users.noreply.github.com>
2022-06-23 13:29:00 +0200

    Add working code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use devzat_rs::Client;
+use devzat_rs::{Client, Listener};
 use std::env;
+use tokio::try_join;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,7 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Err(_) => panic!("Missing PLUGIN_TOKEN"),
     };
 
-    let mut client = Client::new(instance_host, auth_token).await?;
+    let client = Client::new(instance_host, auth_token).await?;
 
     client
         .send_message(
@@ -24,11 +25,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .await?;
 
-    client
-        .register_cmd("greet", "Greet someone.", "<name>", |event| async move {
-            format!("Hello {}!", event.args)
-        })
-        .await?;
+    let listener = Listener {
+        middleware: None,
+        once: None,
+        regex: None,
+    };
+
+    let fut = client.register_listener(listener, |event| async move {
+        eprintln!(
+            "room={}, from={}, msg={}",
+            event.room, event.from, event.msg
+        );
+
+        None
+    });
+
+    let fut2 = client.register_cmd("greet", "Greet someone.", "<name>", |event| async move {
+        format!("Hello {}!", event.args)
+    });
+
+    let fut3 = client.register_cmd("bye", "Say bye to someone.", "<name>", |event| async move {
+        format!("Bye {}!", event.args)
+    });
+
+    let _ = try_join!(fut, fut2, fut3);
 
     Ok(())
 }
